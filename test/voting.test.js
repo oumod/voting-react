e1476050cdba3075e6ec5ed3975cf7778d7af9ef
Oudom Suos <oudom.suos@gmail.com>
2021-03-28 23:31:04 +0200

    Ajout des TUs des 4 dernières fonctions

--- test/voting.test.js
+++ test/voting.test.js
@@ -223,4 +223,210 @@ contract("Voting", accounts => {
             newStatus: "2"
         });
     });
+
+    // Test de `function startVotingSession() external onlyAdmin`
+    it("startVotingSession() doit revert si le sender n'est pas l'admin", async () => {
+        // GIVEN
+        const sender = electeur1;
+
+        // WHEN
+        // THEN
+        await expectRevert(this.votingInstance.startVotingSession({ from: sender }), "Admin requis");
+    });
+    it("startVotingSession() doit revert si le workflow n'est pas au status ProposalsRegistrationEnded", async () => {
+        // GIVEN
+        const status = await this.votingInstance.getWorkflowStatus();
+
+        // WHEN
+        expect(status).to.be.bignumber.not.equal(new BN(2));
+
+        // THEN
+        await expectRevert(this.votingInstance.startVotingSession({ from: admin }), "Enregistrement des propositions pas terminee");
+    });
+    it("startVotingSession() doit émettre 2 évènements, VotingSessionStarted et WorkflowStatusChange", async () => {
+        // GIVEN
+        await this.votingInstance.register(electeur1, { from: admin });
+        await this.votingInstance.startProposalsRegistration({ from: admin });
+        await this.votingInstance.registerProposal("proposition 1", { from: electeur1 });
+        await this.votingInstance.endProposalsRegistration({ from: admin });
+
+        // WHEN
+        const receipt = await this.votingInstance.startVotingSession({ from: admin });
+
+        // THEN
+        expectEvent(receipt, 'VotingSessionStarted');
+        expectEvent(receipt, 'WorkflowStatusChange', {
+            previousStatus: "2",
+            newStatus: "3"
+        });
+    });
+
+    // Test de `function vote(uint proposalId) external isWhitelisted`
+    it("vote(proposalId) doit revert si le sender n'est pas whitelisté", async () => {
+        // GIVEN
+        const sender = electeur1;
+        const proposalId = 0;
+        // Personne dans la whitelist
+
+        // WHEN
+        // THEN
+        await expectRevert(this.votingInstance.vote(proposalId, { from: sender }), "Pas inscrit");
+    });
+    it("vote(proposalId) doit revert si le workflow n'est pas au status VotingSessionStarted", async () => {
+        // GIVEN
+        const sender = electeur1;
+        const proposalId = 0;
+        // sender enregistré
+        await this.votingInstance.register(sender, { from: admin });
+        // le workflow est au status initial
+
+        // WHEN
+        // THEN
+        const status = await this.votingInstance.getWorkflowStatus();
+        expect(status).to.be.bignumber.not.equal(new BN(3));
+        await expectRevert(this.votingInstance.vote(proposalId, { from: sender }), "Vote pas en cours");
+    });
+    it("vote(proposalId) doit revert si le sender a déjà voté", async () => {
+        // GIVEN
+        const sender = electeur1;
+        const proposalId = 0;
+        await this.votingInstance.register(sender, { from: admin });
+        await this.votingInstance.startProposalsRegistration({ from: admin });
+        await this.votingInstance.registerProposal("proposition 1", { from: sender });
+        await this.votingInstance.endProposalsRegistration({ from: admin });
+        await this.votingInstance.startVotingSession({ from: admin });
+        // sender vote une 1ère fois
+        await this.votingInstance.vote(proposalId, { from: sender });
+
+        // GIVEN
+        // THEN
+        // sender vote une 2ème fois
+        await expectRevert(this.votingInstance.vote(proposalId, { from: sender }), "Deja vote");
+    });
+    it("vote(proposalId) doit revert proposalId est invalide", async () => {
+        // GIVEN
+        const sender = electeur1;
+        const proposalId = 0;
+        await this.votingInstance.register(sender, { from: admin });
+        await this.votingInstance.startProposalsRegistration({ from: admin });
+        // Pas de proposition, donc proposalId est invalide quelque soit sa valeur
+        await this.votingInstance.endProposalsRegistration({ from: admin });
+        await this.votingInstance.startVotingSession({ from: admin });
+
+        // GIVEN
+        // THEN
+        await expectRevert(this.votingInstance.vote(proposalId, { from: sender }), "Id de proposition invalide");
+    });
+    it("vote(proposalId) doit incrémenter le nombre de votes pour proposalId et émettre 1 évènement Voted", async () => {
+        // GIVEN
+        const sender = electeur1;
+        const proposalId = 0;
+        await this.votingInstance.register(sender, { from: admin });
+        await this.votingInstance.startProposalsRegistration({ from: admin });
+        await this.votingInstance.registerProposal("proposition 1", { from: sender });
+        await this.votingInstance.endProposalsRegistration({ from: admin });
+        await this.votingInstance.startVotingSession({ from: admin });
+        const voteCountBefore = (await this.votingInstance.getProposal())[0].voteCount;
+
+        // WHEN
+        const receipt = await this.votingInstance.vote(proposalId, { from: sender });
+
+        // THEN
+        const voteCountAfter = (await this.votingInstance.getProposal())[0].voteCount;
+        expect(voteCountAfter).to.be.bignumber.equal(new BN(voteCountBefore + 1));
+        expectEvent(receipt, 'Voted', {
+            voter: sender,
+            proposalId: new BN(proposalId)
+        });
+    });
+
+    // Test de `function endVotingSession() external onlyAdmin`
+    it("endVotingSession() doit revert si le sender n'est pas l'admin", async () => {
+        // GIVEN
+        const sender = electeur1;
+
+        // WHEN
+        // THEN
+        await expectRevert(this.votingInstance.endVotingSession({ from: sender }), "Admin requis");
+    });
+
+    // Test de `function endVotingSession() external onlyAdmin`
+    it("endVotingSession() doit revert si le sender n'est pas l'admin", async () => {
+        // GIVEN
+        const sender = electeur1;
+
+        // WHEN
+        // THEN
+        await expectRevert(this.votingInstance.endVotingSession({ from: sender }), "Admin requis");
+    });
+    it("endVotingSession() doit revert si le workflow n'est pas au status VotingSessionStarted", async () => {
+        // GIVEN
+        const status = await this.votingInstance.getWorkflowStatus();
+
+        // WHEN
+        expect(status).to.be.bignumber.not.equal(new BN(3));
+
+        // THEN
+        await expectRevert(this.votingInstance.endVotingSession({ from: admin }), "Vote pas en cours");
+    });
+    it("endVotingSession() doit émettre 2 évènements, ProposalsRegistrationEnded et WorkflowStatusChange", async () => {
+        // GIVEN
+        await this.votingInstance.register(electeur1, { from: admin });
+        await this.votingInstance.startProposalsRegistration({ from: admin });
+        await this.votingInstance.registerProposal("proposition 1", { from: electeur1 });
+        await this.votingInstance.endProposalsRegistration({ from: admin });
+        await this.votingInstance.startVotingSession({ from: admin });
+        await this.votingInstance.vote(0, { from: electeur1 });
+
+        // WHEN
+        const receipt = await this.votingInstance.endVotingSession({ from: admin });
+
+        // THEN
+        expectEvent(receipt, 'VotingSessionEnded');
+        expectEvent(receipt, 'WorkflowStatusChange', {
+            previousStatus: "3",
+            newStatus: "4"
+        });
+    });
+
+    // Test de `function endProposalsRegistration() external onlyAdmin`
+    it("tally() doit revert si le sender n'est pas l'admin", async () => {
+        // GIVEN
+        const sender = electeur1;
+
+        // WHEN
+        // THEN
+        await expectRevert(this.votingInstance.tally({ from: sender }), "Admin requis");
+    });
+    it("tally() doit revert si le workflow n'est pas au status VotingSessionEnded", async () => {
+        // GIVEN
+        // WHEN
+        const status = await this.votingInstance.getWorkflowStatus();
+
+        // THEN
+        expect(status).to.be.bignumber.not.equal(new BN(5));
+        await expectRevert(this.votingInstance.tally({ from: admin }), "Vote pas termine");
+    });
+    it("tally() doit déterminer la proposition gagnante, et émettre 2 évènements, VotesTallied et WorkflowStatusChange", async () => {
+        // GIVEN
+        await this.votingInstance.register(electeur1, { from: admin });
+        await this.votingInstance.startProposalsRegistration({ from: admin });
+        await this.votingInstance.registerProposal("proposition 1", { from: electeur1 });
+        await this.votingInstance.endProposalsRegistration({ from: admin });
+        await this.votingInstance.startVotingSession({ from: admin });
+        await this.votingInstance.vote(0, { from: electeur1 });
+        await this.votingInstance.endVotingSession({ from: admin });
+
+        // WHEN
+        const receipt = await this.votingInstance.tally({ from: admin });
+
+        // THEN
+        const winningProposalId = await this.votingInstance.winningProposalId();
+        expect(winningProposalId).to.be.bignumber.equal(new BN(0));
+        expectEvent(receipt, 'VotesTallied');
+        expectEvent(receipt, 'WorkflowStatusChange', {
+            previousStatus: "4",
+            newStatus: "5"
+        });
+    });
 });
