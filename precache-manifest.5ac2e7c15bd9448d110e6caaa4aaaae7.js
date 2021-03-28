7654fb3e62068cb1ffb083072080253c4e92de23
Oudom Suos <oudom.suos@gmail.com>
2021-03-29 01:05:42 +0200

    Updates

---
+++ precache-manifest.5ac2e7c15bd9448d110e6caaa4aaaae7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01c9cce7dfeda54b153d3150adc0bdd9",
+    "url": "/voting-react/index.html"
+  },
+  {
+    "revision": "a70f05d7a21eba10ce90",
+    "url": "/voting-react/static/css/2.37c27f5c.chunk.css"
+  },
+  {
+    "revision": "833af1cd6fa6de91d94f",
+    "url": "/voting-react/static/css/main.3d9e9d1a.chunk.css"
+  },
+  {
+    "revision": "a70f05d7a21eba10ce90",
+    "url": "/voting-react/static/js/2.db3e547e.chunk.js"
+  },
+  {
+    "revision": "833af1cd6fa6de91d94f",
+    "url": "/voting-react/static/js/main.b00865d9.chunk.js"
+  },
+  {
+    "revision": "888a65a9359fc8a74ddf",
+    "url": "/voting-react/static/js/runtime-main.be82cffd.js"
+  }
+]);
